ac1def998f352e8cc08ec3cb89db58ddc2004787
Scanlon <ScanlonJ@rothschild.com>
2016-09-26 16:55:27 +0100

    Added the login details to provide to the service call

--- src/app/pages/dashboard/availableServices/availableServices.component.ts
+++ src/app/pages/dashboard/availableServices/availableServices.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { ServicesAvailableService } from './servicesAvailable.service';
 import { HealthCheckService } from '../../../services/healthcheck/healthcheck-service';
 import { ServiceDetail} from '../../../model/serviceDetail';
+import { LocalStorageService } from 'angular-2-local-storage';
 
 @Component({
   selector: 'available-services',
@@ -16,7 +17,9 @@ export class AvailableServices {
   public availableServices: Array<ServiceDetail>;
   private _init = false;
 
-  constructor(private _servicesAvailableService: ServicesAvailableService) {
-    this.availableServices = this._servicesAvailableService.getAvailableServices();
+  constructor(private _servicesAvailableService: ServicesAvailableService, private _localStorage: LocalStorageService) {
+    let loginDetails = this._localStorage.get('loginDetails');
+    let userName = loginDetails['userName'];
+    this.availableServices = this._servicesAvailableService.getAvailableServices(userName);
   }
 }
