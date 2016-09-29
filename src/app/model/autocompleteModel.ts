439cf06098015431ebf66c63bc439a25ea0c14ca
Scanlon <ScanlonJ@rothschild.com>
2016-09-29 17:10:25 +0100

    committed changes to get the information for the autocomplete

---
+++ src/app/model/autocompleteModel.ts
@@ -0,0 +1,10 @@
+export class AutoCompleteModel {
+    public _equityName: string;
+    public _equityTickerRegion: string;
+    constructor(equityName: string, equityTickerRegion: string) {
+        this._equityName = equityName;
+        this._equityTickerRegion = equityTickerRegion;
+        
+    }
+
+}
--- src/app/pages/components/components/currentAnalysis/current-analysis.component.ts
+++ src/app/pages/components/components/currentAnalysis/current-analysis.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewEncapsulation, NgModule } from '@angular/core';
-
+import { AutoCompleteModel } from '../../../../model/autoCompleteModel';
 import { LocalStorageService } from 'angular-2-local-storage';
+import { EquityService } from '../../../../services/equities/equity-service';
 
 
 @Component({
@@ -13,14 +14,36 @@ import { LocalStorageService } from 'angular-2-local-storage';
 
 export class CurrentAnalysis {
 
-  constructor(
-  private _localStorage: LocalStorageService) {
+  private results: AutoCompleteModel[];
+  private selectedEquityResult: AutoCompleteModel;
+  private equityModel: AutoCompleteModel;
+  private username:string;
+  private showHolderPositions: boolean;
+  private resultLimit: string = "30";
+  constructor(private _equityService: EquityService, private _localStorage: LocalStorageService) {
+
     let loginDetails = this._localStorage.get('loginDetails');
-    let userName = loginDetails['userName']
-    
+    this.username = loginDetails['userName'];
+    this.showHolderPositions = false;
   }
 
-  save() {
-        // do something..
-    }
+  /*
+    this method needs to get all the reportable equities which we have retrieved
+    with the isCalculable Equities.
+  */
+  
+  findEquityMatches(event) {
+    let equityValue = <string>event.query;
+    this._equityService.getEquityListByTerm(equityValue, this.resultLimit).subscribe(res => res => {
+      this.results = res;
+    })
+  }
+
+  selectedEquity(value) {
+    this.selectedEquityResult = value;
+    
+    // so when we select the equity we then need to pass this on to whatever is going to call the code to populate the 
+    // holders by institution and their positions
+  }
+
 }
